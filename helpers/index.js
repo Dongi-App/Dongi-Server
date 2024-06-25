c5dda7e00462c68aa04dabf09983e0fd743f504e
Mehdi Abdi <abdimehdi1321@gmail.com>
2024-06-25 13:41:07 +0330

    helpers: add boolSerializer

--- helpers/index.js
+++ helpers/index.js
@@ -22,10 +22,25 @@ const emailSerializer = (email) => {
   return email.toLowerCase();
 };
 
+const boolSerializer = (x) => {
+  if (typeof x === "boolean") {
+    return x;
+  }
+
+  if (x.toLowerCase() === "true") {
+    return true;
+  } else if (x.toLowerCase() === "false") {
+    return false;
+  } else {
+    throw Error("invalid boolean");
+  }
+};
+
 module.exports = {
   findOne,
   insertNewDocument,
   updateDocument,
   deleteDocument,
-  emailSerializer
+  emailSerializer,
+  boolSerializer,
 };
